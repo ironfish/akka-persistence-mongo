04ae1f69a1638c59698954fef9d2b00ff7087bc6
Sean Walsh <swalsh@viridityenergy.com>
2014-03-14 11:06:39 -0400

    Removed become/unbecome

--- akka-persistence-mongo-command-sourcing-example-app/src/main/scala/org/example/Benefits.scala
+++ akka-persistence-mongo-command-sourcing-example-app/src/main/scala/org/example/Benefits.scala
@@ -13,13 +13,10 @@ case class EmployeeBenefits(employeeId: String, startDate: Long, termDates: List
  * This read side representation is interested in maintaining a true employment duration, which takes into account periods of
  * non-employment. We could get fancy here and schedule updates to an employmentLength field if we wanted but for the sake of
  * this sample we'll just accumulate the pertinent dates.
- *
- * This actor shows how to do upsert to a database when you want to ensure ordering, i.e. the creation is guaranteed to
- * occur before any updates.  This is done using akka become and then unbecome after completion of the future mongodb
- * write operation.  Any write operation that fails for any reason will cause an exception and a restart of this actor in
- * the normal state.
  */
 class Benefits(mongoHost: String, mongoPort: Int) extends Actor {
+
+  implicit val writeConcern = WriteConcern.JournalSafe
 
   def receive: Actor.Receive = {
     case p @ ConfirmablePersistent(event, sequenceNr, redeliveries) =>
@@ -28,30 +25,24 @@ class Benefits(mongoHost: String, mongoPort: Int) extends Actor {
 
       event match {
         case cmd: EmployeeHired      =>
-          context become waiting
           val eb = EmployeeBenefits(cmd.id, cmd.startDate, Nil, Nil)
           val dbo = grater[EmployeeBenefits].asDBObject(eb)
           col.insert(dbo)
           p.confirm()
-          context.unbecome()
 
         case cmd: EmployeeTerminated =>
-          context become waiting
           val dbo = col.findOne(MongoDBObject("employeeId" -> cmd.id)).get
           val eb = grater[EmployeeBenefits].asObject(dbo)
           val up = eb.copy(termDates = eb.termDates :+ cmd.termDate)
           col.update(MongoDBObject("employeeId" -> cmd.id), grater[EmployeeBenefits].asDBObject(up))
           p.confirm()
-          context.unbecome()
 
         case cmd: EmployeeRehired    =>
-          context become waiting
           val dbo = col.findOne(MongoDBObject("employeeId" -> cmd.id)).get
           val eb = grater[EmployeeBenefits].asObject(dbo)
           val up = eb.copy(rehireDates = eb.rehireDates :+ cmd.rehireDate)
           col.update(MongoDBObject("employeeId" -> cmd.id), grater[EmployeeBenefits].asDBObject(up))
           p.confirm()
-          context.unbecome()
 
         case _ => // Not interested
       }
